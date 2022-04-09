288dd013a28e760b33e618461bd7658bdde85379
SITI FAUZIAH <91424902+sitifauziahh05@users.noreply.github.com>
2022-04-09 23:29:18 +0700

    Create Abdullah.php

---
+++ Abdullah.php
@@ -0,0 +1,11 @@
+$strings = array (
+'Saya adalah Abdullah',
+'Saya punya seekor kucing yang sangat lucu',
+'Setiap hari ku ajak dia bermain' 
+);
+$search = array (
+'Abdullah',
+'kucing',
+'lucu',
+'bermain' 
+);
